a157ef13fdab06227abe50128cb52c6085c69424
maor gigi <maorgigim1509@gmail.com>
2023-06-09 18:57:25 +0300

    Add Randomize Button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
+var button = document.querySelector("button");
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
@@ -13,7 +13,28 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+function setRandomColor(){
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ generateRandomHexColor() 
+	+ ", " 
+	+ generateRandomHexColor()
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+function generateRandomHexColor() { // Generating random hex-value colors
+	var x = Math.round(0xffffff * Math.random()).toString(16);
+	var y = (6 - x.length);
+	var z = "000000";
+	var z1 = z.substring(0, y);
+	var randomColor = '#' + z1 + x;
+	return randomColor;
+}
+
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click",setRandomColor);
